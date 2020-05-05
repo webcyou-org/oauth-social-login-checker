f069dd764871c3d273f5d94bae545b4768e7f5a7
panicdragon <d.takayama.web@gmail.com>
2020-05-05 12:10:02 +0900

    refactor: 💡 Provider class

--- lib/class/Provider.ts
+++ lib/class/Provider.ts
@@ -1,11 +1,21 @@
 export class Provider {
     public name: string
     public clientId: string
+    public redirectUri: string
+    public responseType: string
+    public scope: string
+    public state: string
+    public nonce: string
 
     constructor(data?: any) {
         if (!data) data = {}
 
         this.name = data.name || ''
         this.clientId = data.client_id || ''
+        this.redirectUri = data.redirect_uri || 'http://localhost:3000/callback'
+        this.responseType = data.response_type || ''
+        this.scope = data.scope || ''
+        this.state = data.state || ''
+        this.nonce = data.nonce || ''
     }
 }
